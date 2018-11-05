ee40f027e01bf1bdb602f32c9e20bf8884fd9b90
vnadezda <36225667+vnadezda@users.noreply.github.com>
2018-11-05 11:02:31 +0200

    Initial commit
    Commit

---
+++ CompanyManagementSystem_employee/CompanyManagementSystem_employee/Controllers/EmployeeController.cs
@@ -0,0 +1,101 @@
+﻿using CompanyManagementSystem_employee.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CompanyManagementSystem_employee.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Employee
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ViewAll()
+        {
+            return View(GetAllEmployee());
+        }
+
+        IEnumerable<Employee> GetAllEmployee()
+        {
+            using (DBModel db = new DBModel())
+            {
+                return db.Employees.ToList<Employee>();
+            }
+        }
+
+        [HttpGet]
+        public ActionResult AddOrEdit(int id = 0)
+        {
+            Employee emp = new Employee();
+            if (id != 0)
+            {
+                using (DBModel db = new DBModel())
+                {
+                    if (emp.EmployeeId == 0)
+                    {
+                        emp = db.Employees.Where(x => x.EmployeeId == id).FirstOrDefault<Employee>();
+                    }
+                    else
+                    {
+                        db.Entry(emp).State = EntityState.Modified;
+                        db.SaveChanges();
+                    }
+                }
+            }
+            return View(emp);
+        }
+
+        //HttpPost AddOrEdit – Form returned from HttpGet AddOrEdit will be posted to this action method.
+        [HttpPost]
+        public ActionResult AddOrEdit(Employee emp)
+        {
+            try
+            {
+                using (DBModel db = new DBModel())
+                {
+                    if (emp.EmployeeId == 0)
+                    {
+                        db.Employees.Add(emp);
+                        db.SaveChanges();
+                    }
+                    else
+                    {
+                        db.Entry(emp).State = EntityState.Modified;
+                        db.SaveChanges();
+                    }
+                }
+                return Json(new { success = true, html = GlobalClass.RenderRazorViewToString(this, "ViewAll", GetAllEmployee()), message = "Submitted Successfully" }, JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception ex)
+            {
+                return Json(new { success = false, message = ex.Message }, JsonRequestBehavior.AllowGet);
+            }
+        }
+
+        //HttpPost Delete – Used to Delete an employee record with given EmployeeID through id parameter.
+        [HttpPost]
+        public ActionResult Delete(int id)
+        {
+            try
+            {
+                using (DBModel db = new DBModel())
+                {
+                    Employee emp = db.Employees.Where(x => x.EmployeeId == id).FirstOrDefault<Employee>();
+                    db.Employees.Remove(emp);
+                    db.SaveChanges();
+                }
+                return Json(new { success = true, html = GlobalClass.RenderRazorViewToString(this, "ViewAll", GetAllEmployee()), message = "Deleted Successfully" }, JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception ex)
+            {
+                return Json(new { success = false, message = ex.Message }, JsonRequestBehavior.AllowGet);
+            }
+        }
+    }
+}
